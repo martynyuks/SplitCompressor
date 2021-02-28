b81bf52fb9a624c630530dbedd5bba2aad8674ad
Sergey Martynyuk <martynyuk.s.a@gmail.com>
2021-02-28 15:48:04 +0300

    Создан проект утилиты.

---
+++ SplitCompressor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SplitCompressor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
